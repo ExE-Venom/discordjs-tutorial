6acec8af30aa00fadae5fb9808d43fd522e99dce
VenomExE <121004003+ExE-Venom@users.noreply.github.com>
2023-03-06 11:31:06 +0530

    Update commands.js

--- src/handlers/commands.js
+++ src/handlers/commands.js
@@ -1,1 +1,19 @@
+const fs = require("fs");
 
+module.exports = (client, config) => {
+
+  fs.readdirSync('./src/commands/normal/').forEach(dir => {
+    const commands = fs.readdirSync(`./src/commands/normal/${dir}`).filter(file => file.endsWith('.js'));
+    for (let file of commands) {
+
+      let pull = require(`../commands/normal/${dir}/${file}`);
+      if (pull.config.name) {
+        client.commands.set(pull.config.name, pull);
+      } else {
+        console.log(`[HANDLER] Couldn't load the file ${file}, missing name.`)
+        continue;
+      };
+
+    };
+  });
+};
