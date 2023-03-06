8a973ebd86234a59028627ccf3254f9fa9107d6a
VenomExE <121004003+ExE-Venom@users.noreply.github.com>
2023-03-06 11:25:30 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,19 +1,31 @@
-const { Client, Collection } = require('discord.js');
+const { Client, Partials, Collection, GatewayIntentBits } = require('discord.js');
 const fs = require('fs');
 const env = process.env;
 const config = require('./config.json');
 
 const client = new Client(
-        intents: ClientIntents,
-        partials: ClientPartials,
-        presence: {
-            activities: [{
-                name: 'VenomExE Tutorial',
-                type: 0
-            }],
-            status: 'online'
-        }
-    }
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.GuildPresences,
+    GatewayIntentBits.GuildMessageReactions,
+    GatewayIntentBits.DirectMessages,
+    GatewayIntentBits.MessageContent
+  ],
+  partials: [
+    Partials.Channel,
+    Partials.Message,
+    Partials.User,
+    Partials.GuildMember,
+    Partials.Reaction
+  ],
+  presence: {
+    activities: [{
+        name: 'VenomExE Tutorial',
+        type: 0
+    }],
+    status: 'online'
+  }
 )
 
 client.commands = new Collection();
@@ -23,9 +35,9 @@ module.exports = client;
 module.exports = config;
 module.exports = env;
 
-fs.readdirSync('./src/handlers').forEach((handler) => {
-    console.log('[HANDLER] Loaded: ' + handler);
-    require('./handlers/' + handler)(client, config);
+fs.readdirSync('./src/handlers').forEach((file) => {
+    console.log('[HANDLER] Loaded: ' + file);
+    require('./handlers/' + file)(client, config);
 });
 
 client.login(env.BOT_TOKEN);
